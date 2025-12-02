598da44595f309c7a39ddd1f0554b0834b0cd930
Javohir <javohirzikirov2@gmail.com>
2025-12-03 00:00:32 +0500

    boolean bn ishlash

---
+++ boolean.py
@@ -0,0 +1,40 @@
+"""Boolean ma'lumot turi, Operatorlar"""
+"""
+Solishtrish operatorlari:
+   == (tengmi?)
+   != (teng emasmi?)
+   > (katta)
+   < (kichik)
+   >= (katta yoki teng)
+   <= (kichik yoki teng)
+"""
+"""
+Mantiqiy operatorlar:
+   and: ikkala shart ham True bo'lsa, natija True.
+   or: biror bir shart True bo'lsa, natija True.
+   not: qiymatni teskarisiga o'zgartiradi (True ni False ga va aksincha).
+"""
+
+# print(4==44)
+# print(4!=44)
+# print(4>44)
+# print(4<44)
+# print(4>=44)
+# print(4<=44)
+
+# print((3<4) and (6<9))
+# print((3>4) or (6<9))
+# print( not (6<9))
+
+
+# print(4==77)
+# print(4!=77)
+# print(4>77)
+# print(4<77)
+# print(4>=77)
+# print(4<=77)
+
+# print((9<5) and (5>8))
+# print((3>7) or (7>4))
+print( not (8<9))
+
