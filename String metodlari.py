409d6129e345797939c1a06643c88674fa1e42ac
Javohir <javohirzikirov2@gmail.com>
2025-12-02 23:29:24 +0500

    String metodlari

---
+++ String metodlari.py
@@ -0,0 +1,65 @@
+"""String metodlari"""
+
+
+"""capitalize  
+casefold    
+center      
+count       
+encode      
+endswith    
+expandtabs  
+find        
+format      
+format_map  
+index       
+isalnum     
+isalpha     
+isascii     
+isdecimal   
+isdigit     
+isidentifier
+islower     
+isnumeric
+isprintable
+isspace
+istitle
+isupper
+join
+ljust
+lower
+lstrip
+maketrans
+partition
+removeprefix
+removesuffix
+replace
+rfind
+rindex
+rjust
+rpartition
+rsplit
+rstrip
+split
+splitlines
+startswith
+strip
+swapcase
+title
+translate
+upper
+zfill
+"""
+
+course = "Python is very cool cool"
+# print(course.upper())
+# print(course.lower())
+# print(course.capitalize())
+# print(course.title())
+# print(course.strip())
+# print(course.startswith(" "))
+# print(course.endswith("n"))
+# print("Pyt" in course)
+# print(course.replace("cool", "perfect"))
+# print(len(course))
+# print(course.find("s"))
+# print(course.count("cool"))
