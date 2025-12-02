3a74e2049a523ae27c3bfdc9b09605c168ef5a6b
Javohir <javohirzikirov2@gmail.com>
2025-12-02 23:02:23 +0500

    Stringlar bilan ishlash

---
+++ Stringlar.py
@@ -0,0 +1,24 @@
+"""Stringlar bn ishlash"""
+
+# matn = "Salom bolalar"
+# matn2 ="Python dasturlash tili"
+# print(f"{type(matn)}: {matn}")
+# print(f"{type(matn2)}: {matn2}")
+
+# ism = input("ismiz nima:")
+# salom =f"Assalom aleykum {ism}"
+# uzunlik =f"sizning ismizni uzunligi: {len(ism)} ta"
+# print(uzunlik)
+
+#kesma   [start: stop: step]
+#       0123456
+matn = "Asadbek"
+birinchi_harf = matn[0]
+oxirgi_harf = matn [-1]
+
+a = matn[::-1]
+
+
+print(a)
+# print(f"birinchi_harf: {birinchi_harf}")
+# print(f"oxirgi_harf: {oxirgi_harf}")
