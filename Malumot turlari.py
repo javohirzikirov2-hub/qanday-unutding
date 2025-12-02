97f645e46aa7944cca361e7826771e2afc4412e1
Javohir <javohirzikirov2@gmail.com>
2025-12-02 22:14:33 +0500

    input, casting

---
+++ Malumot turlari.py
@@ -0,0 +1,30 @@
+""" Ma'lumot turlari, input(), casting """
+"""
+   integer: butun sonlar uchun : 1, 2, 3
+   float: o'nlik sonlar uchun: 1.5, 3.5
+   string: matn uchun "hello", "Java"
+   boolen: mantiqiy True, False
+"""
+
+yosh = 58 #int
+boy = 1.79 #float
+ism = "Cristiano"  #str
+togrimi = True #bool
+
+
+# ism = input("Ismiz nima:")
+# print("Salom " + ism)
+# print(type(ism))
+
+# yosh = int(input("Yoshiz nechida: "))
+# print(f"Yosh: {yosh + 13}")
+# print(type(yosh))
+
+# boy = float(input("Bo'yiz nechchi: "))
+# print(f"Bo'y: {boy}")
+# print(type(boy))
+
+togrimi = bool(input("Shu To'grimi: "))
+print(f"To'grimi: {togrimi}")
+print(type(togrimi))
+
