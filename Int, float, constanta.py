43b718d40ffe19b7d68c878603a5ffefbf1a46f7
Javohir <javohirzikirov2@gmail.com>
2025-12-02 22:39:12 +0500

    Int, float, constanta

---
+++ Int, float, constanta.py
@@ -0,0 +1,46 @@
+""" Int, float, constanta. """
+
+""" Int: integer """
+# a = 10 
+# b = -3
+# c = a + b
+
+# print(f"Butun sonlar: a: {a} b:{b}")
+# print(f"Ularning yigindisi: {c}")
+
+# d = 18
+# e = -6
+# f = d + e
+
+# print(f"Butun sonlar: d: {d} e:{e}")
+# print(f"Ularning yigindisi: {f}")
+
+
+
+
+"""Float"""
+# x = 4.5
+# y = 5.9
+# z = x * y
+# print(f"O'nlik sonlar: x: {x} y:{y}")
+# print(f"Ularning yigindisi: {z}")
+
+# j = 8.7
+# h = 5.6
+# l = j * h
+# print(f"O'nlik sonlar: j: {j} h:{h}")
+# print(f"Ularning yigindisi: {l}")
+
+
+"""constanta"""
+# PI = 3.14
+# GRAVITY = 9.81
+
+# print(f"PI: {PI}")
+# print(f"Yerning tortishish kuchi: {GRAVITY}")
+
+# MESSI = 5.77
+# RONALDO = 8.77
+
+# print(f"MESSI: {MESSI}")
+# print(f"RONALDO: {RONALDO}")
